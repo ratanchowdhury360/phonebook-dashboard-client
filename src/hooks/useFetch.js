e8972419885bfc25f6bd5a8e0b33b66e5165a9a4
Md. Ratan Chowdhury <ratanchowdhury169@gmail.com>
2026-02-04 22:40:29 +0600

    added hooks for all component data fetch

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+
+const useFetch = (url) => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((result) => {
+        setData(result);
+        setLoading(false);
+      })
+      .catch(() => setLoading(false));
+  }, [url]);
+
+  return { data, loading };
+};
+
+export default useFetch;
